3dfdd24ac7bc7f3300c99aac4ab862acd0cfd748
akyreisme <1924143976@qq.com>
2019-11-20 15:10:35 +0800

    add

--- Longest_String_leetcode_3.java
+++ Longest_String_leetcode_3.java
@@ -10,12 +10,12 @@ public class Longest_subString {
         int n = s.length();
         int ans = 0;
         HashMap<Character, Integer> map = new HashMap<>();
-        int max = 0;
+        int subscript = 0;
         for (int j = 0; j < n; j++) {
             if (map.containsKey(s.charAt(j))) {
-                max = Math.max(map.get(s.charAt(j)), max);//找到前一位置处的相同字符
+                subscriptax = Math.max(map.get(s.charAt(j)), subscript);//找到前一位置处的相同字符
             }
-            ans = Math.max(ans, j - max + 1);
+            ans = Math.max(ans, j - subscript + 1);
             map.put(s.charAt(j), j + 1);
         }
         return ans;

86d081827550ea50efa11cfb68415088c2b6182f
akyreisme <1924143976@qq.com>
2019-12-19 22:05:04 +0800

    add

---
+++ LeetCode/numJewlsInStones.java
@@ -0,0 +1,12 @@
+public int numJewelsInStones(String J, String S) {
+        int sum = 0;
+        for(int i = 0; i < J.length(); i++){
+            for(int j = 0;j < S.length(); j++){
+                if(J.charAt(i)==S.charAt(j)){
+                    sum++;
+                }
+            }
+           
+        }
+         return sum;
+    }

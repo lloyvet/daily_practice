cf6ef50c5a2af8683c55be5c46327418c29017e3
akyreisme <1924143976@qq.com>
2020-02-13 22:42:29 +0800

    add

---
+++ LeetCode/heightChecker.java
@@ -0,0 +1,13 @@
+public int heightChecker(int[] heights) {
+        int[] arr = new int[heights.length];
+        for(int i = 0; i < heights.length; i++){
+            arr[i] = heights[i];
+        }
+        Arrays.sort(arr);
+        int num = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]!=heights[i])
+                num++;
+        }
+        return num;
+    }
